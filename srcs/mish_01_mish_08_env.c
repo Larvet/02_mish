25359e34996286a3384034460689fad1e23d4f12
Louis Charvet <locharve@1D6.42angouleme.fr>
2024-05-08 18:18:04 +0200

    not much

--- srcs/mish_01_mish_08_env.c
+++ srcs/mish_01_mish_08_env.c
@@ -6,7 +6,7 @@
 /*   By: bchene <bchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 15:13:08 by bchene            #+#    #+#             */
-/*   Updated: 2024/04/27 17:42:18 by bchene           ###   ########.fr       */
+/*   Updated: 2024/05/08 16:31:24 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,32 @@ int	mish_env_remove(t_mish *mish, char *var)
 	{
 		t_env_remove(&(mish->env), tenv);
 		return (1);
+	}
+	return (0);
+}
+
+int	mish_env_change_value(t_mish *mish, t_env *env_or_u, char *var, char *value)
+{
+	t_env	*to_change;
+	char	*str;
+	size_t	len_var;
+
+	to_change = t_env_getvar(env_or_u, var);
+	if (to_change)
+	{
+		len_var = ft_strlen(var);
+		str = ft_calloc(len_var + 1 + ft_strlen(value) + 1, sizeof(char));
+		if (str)
+		{
+			ft_strcpy(str, var);
+			str[len_var] = '=';
+			ft_strcpy(&str[len_var + 1], value);
+			free(to_change->str);
+			to_change->str = str;
+		}
+		else
+			return (mish_error_add(mish, err_malloc,
+					errno, "mish_env_change_value"));
 	}
 	return (0);
 }
